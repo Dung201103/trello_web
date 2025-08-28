6749f964073f9da77aea54dec8941ef4b5356114
Dung201103 <21t1020322@husc.edu.vn>
2025-08-28 21:19:28 +0700

    Cau truc lai du an chia component

---
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -0,0 +1,22 @@
+
+import Box from '@mui/material/Box'
+import ListColumns from './ListColumns/ListColumns'
+
+
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
+function BoardContent() {
+
+  return (
+    <Box sx={{
+      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+      width: '100%',
+      height: (theme) => theme.trello.boardContentHeight,
+      p: '10px 0'
+    }}>
+      <ListColumns />
+    </Box>
+  )
+}
+
+export default BoardContent
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,7 +1,7 @@
 import Container from '@mui/material/Container'
-import AppBar from '../../components/Appbar'
-import BoarBar from './BoarBar'
-import BoardContent from './BoardContent'
+import AppBar from '~/components/AppBar/AppBar'
+import BoarBar from './BoarBar/BoarBar'
+import BoardContent from './BoardContent/BoardContent'
 function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
